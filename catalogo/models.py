87c154d03c26a4933aac8050929c998eebea01ce
joaovrds15 <joaovictorodrigues4009@gmail.com>
2023-02-12 11:38:30 -0300

    feat: adding models and migrations

--- catalogo/models.py
+++ catalogo/models.py
@@ -1,8 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    nome = models.CharField(max_length=56)
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
 class Country(models.Model):
     nome = models.CharField(max_length=56)
+
+class Objective(models.Model):
+    nome = models.CharField(max_length=56)
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
 class Product(models.Model):
     pais = models.ForeignKey(
@@ -11,11 +20,28 @@ class Product(models.Model):
         blank=False,
         null = False,
     )
-    nome = models.CharField(max_length=56)
+    nome = models.CharField(max_length=56) 
     descricao = models.TextField()
     conservacao = models.TextField()
     sugestao_de_uso = models.TextField()
-    image_url = models.CharField(max_length=255)
+    categories = models.ManyToManyField(Category)
+    objectives = models.ManyToManyField(Objective)
+    image_url = models.CharField(max_length=255, null=True)
     modo_uso = models.TextField()
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Lote(models.Model):
+    produto = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE,
+        blank=False,
+        null = False,
+    )
+    numero = models.IntegerField()
+    preco = models.DecimalField(max_digits=6, decimal_places=2)
+    unidade_medida = models.CharField(max_length=3)
+    data_fabricacao = models.DateField()
+    data_vencimento = models.DateField()
     created_at = models.DateTimeField(auto_now=True)
     updated_at = models.DateTimeField(auto_now=True)
